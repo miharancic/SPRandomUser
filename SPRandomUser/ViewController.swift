30eb9451d020706c1f183459961ed9bd6b8bc7eb
Mihailo Rancic <mihailo.rancic@gmail.com>
2019-11-05 21:32:46 +0100

    Initial commit

---
+++ SPRandomUser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPRandomUser
+//
+//  Created by Mihailo Rancic on 05/11/2019.
+//  Copyright © 2019 Mihailo Rancic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
