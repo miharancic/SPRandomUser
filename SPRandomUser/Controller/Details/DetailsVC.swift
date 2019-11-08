bf3cf5dfe60eb44f905e9dd916585f194366da2e
Mihailo Rancic <mihailo.rancic@gmail.com>
2019-11-08 18:51:16 +0100

    Embed UserListTVC in a Navigation Controller, add Details Storyboard and ViewController

---
+++ SPRandomUser/Controller/Details/DetailsVC.swift
@@ -0,0 +1,13 @@
+//
+//  DetailsVC.swift
+//  SPRandomUser
+//
+//  Created by Mihailo Rancic on 08/11/2019.
+//  Copyright © 2019 Mihailo Rancic. All rights reserved.
+//
+
+import UIKit
+
+final class DetailsVC: UIViewController {
+    
+}
